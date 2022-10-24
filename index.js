9b3b99c62c9a7ae9ab9acd25af77da7149826b7b
Megan McHugh <megan.mchugh@gmail.com>
2022-10-23 22:05:33 -0500

    added reset.js and changed setup of questions

--- index.js
+++ index.js
@@ -3,94 +3,111 @@ const jest = require('jest');
 const path = require('path');
 const fs = require('fs');
 const template = require('./src/template');
-const Employee = require('')
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-function buildManager() {
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "What is the manager's name?",
+    },
+    {
+        type: 'input',
+        name: 'managerID',
+        message: "What is the manager's ID?",
+    },
+    {
+        type: 'input',
+        name: 'managerEmailAddress',
+        message: "What is the manager's email address?",
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: "What is the manager's office number?",
+    },
+];
 
-    inquirer.prompt( [
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "What is the manager's name?",
-        },
-        {
-            type: 'input',
-            name: 'managerID',
-            message: "What is the manager's ID?",
-        },
-        {
-            type: 'input',
-            name: 'managerEmailAddress',
-            message: "What is the manager's email address?",
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNumber',
-            message: "What is the manager's office number?",
-        },
-    ]).then
+const employeesQuestion =
+{
+    type: 'list',
+    name: 'addEmployees',
+    message: 'Which employee would you like to add next?',
+    choices: ['Intern',
+        'Engineer']
 };
 
-function buildTeam () {
-    const employeesQuestion = [
-        {
-            type: 'list',
-            name: 'addEmployees',
-            message: 'Which employee would you like to add next?',
-            choices: ['Intern',
-            'Engineer',
-            'Employee',
-    
-        ],
-            },
-    
-    ];
-
-    const engineerQuestions =[
-        {
-            type: 'input',
-            name: 'engineerName',
-            message: "What is the engineer's name?",
-        },
-        {
-            type: 'input',
-            name: 'engineerID',
-            message: "What is the engineer's ID?",
-        },
-        {
-            type: 'input',
-            name: 'engineerEmailAddress',
-            message: "What is the engineer's email address?",
-        },
-        {
-            type: 'input',
-            name: 'engineerOfficeNumber',
-            message: "What is the engineer's office number?",
-        },
-        {
-            type: 'input',
-            name: 'engineerGitHub',
-            message: "What is the engineer's GitHub username?",
-        },
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "What is the engineer's name?",
+    },
+    {
+        type: 'input',
+        name: 'engineerID',
+        message: "What is the engineer's ID?",
+    },
+    {
+        type: 'input',
+        name: 'engineerEmailAddress',
+        message: "What is the engineer's email address?",
+    },
+    {
+        type: 'input',
+        name: 'engineerOfficeNumber',
+        message: "What is the engineer's office number?",
+    },
+    {
+        type: 'input',
+        name: 'engineerGitHub',
+        message: "What is the engineer's GitHub username?",
+    },
 
 ];
 
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: "What is the intern's name?",
+    },
+    {
+        type: 'input',
+        name: 'internID',
+        message: "What is the intern's ID?",
+    },
+    {
+        type: 'input',
+        name: 'internEmailAddress',
+        message: "What is the intern's email address?",
+    },
+    {
+        type: 'input',
+        name: 'internOfficeNumber',
+        message: "What is the intern's office number?",
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: "What is th name of the intern's school?",
+    },
 
-
-};
-
-
+];
 
 // TODO: Create a function to write HTML file
-function writeFile (fileName, data) {
+function writeFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app and call functions to ask questions
+// TODO: Create a function to initialize app and ask questions
 function init() {
     inquirer.prompt(questions).then((inquirerResponses) => {
         console.log('Generating your application');
-        writeFile('team.html', template ({ ...inquirerResponses }));
+        writeFile('team.html', template({ ...inquirerResponses }));
     });
 };
 
