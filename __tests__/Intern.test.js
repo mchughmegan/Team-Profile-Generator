0231e84e3923e6adc4c06a6a7e712802b62f36bd
Megan McHugh <megan.mchugh@gmail.com>
2022-10-25 19:41:46 -0500

    updated Intern class

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,18 @@
+const Intern = require("../lib/Intern")
+describe("I am describing the Initialization of the Intern Class", () => {
+it("When I pass the fourth parameter through the Intern class, it should relate to the school property of the object created", () => {
+    const testedElement = new Intern ("Megan", 12, "email.com", 'UT');
+    expect(testedElement.school).toBe('UT')
+})
+})
+
+describe("I am describing the Methods of the Intern Class", () => {
+    it("Should return the school property when I invoke getSchool()", () => {
+        const testedElement = new Intern("Megan", 14, "email.com", 'UT');
+        expect(testedElement.getSchool()).toBe('UT')
+    })
+    it("Should return Intern when I invoke getRole()", () => {
+        const testedElement = new Intern();
+        expect(testedElement.getRole()).toBe('Intern')
+    })
+})
